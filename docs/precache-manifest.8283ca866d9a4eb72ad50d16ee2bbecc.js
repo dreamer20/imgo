3b4baa5ba88db0cb8bc118e73c5db1b3c9b9b0be
dreamer20 <gv4alex@gmail.com>
2018-12-14 04:59:02 +0300

    Update packages and rebuild project.

---
+++ docs/precache-manifest.8283ca866d9a4eb72ad50d16ee2bbecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4eaae5e074dc4b3f47c8",
+    "url": "/imgo/static/js/runtime~main.4eaae5e0.js"
+  },
+  {
+    "revision": "3d8cff0a8f69329f2646",
+    "url": "/imgo/static/js/main.3d8cff0a.chunk.js"
+  },
+  {
+    "revision": "e5e0eb59313787143232",
+    "url": "/imgo/static/js/1.e5e0eb59.chunk.js"
+  },
+  {
+    "revision": "3d8cff0a8f69329f2646",
+    "url": "/imgo/static/css/main.5c1d90cd.chunk.css"
+  },
+  {
+    "revision": "e5e0eb59313787143232",
+    "url": "/imgo/static/css/1.fcaeee91.chunk.css"
+  },
+  {
+    "revision": "37167ff84dc08306892a51b836c72401",
+    "url": "/imgo/index.html"
+  }
+];
